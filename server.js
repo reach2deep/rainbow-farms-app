3209f63815466413aac724ce5e89e878e2a212d4
Dheeban <dheeban.kumar@gac.com>
2018-07-23 11:19:09 +0800

    build Default path updated

--- server.js
+++ server.js
@@ -23,18 +23,19 @@ const forceSSL = function() {
 // to use the forceSSL
 // middleware
 //app.use(forceSSL());
- 
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
-/*
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-*/
- 
+
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+ 
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+ 
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 4200, function() {
